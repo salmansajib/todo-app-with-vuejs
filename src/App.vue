9cd161555de73483bbd3a3186b03bb6680a2b2ea
Salman-sajib <sfsajib@gmail.com>
2024-08-29 22:12:43 +0600

    removed the border

--- src/App.vue
+++ src/App.vue
@@ -125,13 +125,13 @@ onMounted(() => {
             @submit.prevent="addTodo"
           >
             <input
-              class="bg-slate-100/20 backdrop-blur-[2px] text-slate-100 text-lg grow p-2 rounded-[4px] placeholder:text-slate-500 border border-slate-100"
+              class="bg-slate-100/20 backdrop-blur-[2px] text-slate-100 text-lg grow p-2 rounded-[4px] placeholder:text-slate-500"
               type="text"
               placeholder="add todo here"
               v-model="inputContent"
             />
             <button
-              class="bg-slate-100 hover:bg-slate-200 px-6 py-2 text-lg text-slate-900 font-medium rounded-[4px] cursor-pointer transition-color duration-200 ease-in-out border border-slate-100"
+              class="bg-slate-100 hover:bg-slate-200 px-6 py-2 text-lg text-slate-900 font-medium rounded-[4px] cursor-pointer transition-color duration-200 ease-in-out"
               type="submit"
             >
               Add
